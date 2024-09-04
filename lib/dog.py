cab587976dd8b07ccd8560e7bca71478a0ade31d
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-04 23:35:26 +0300

    Added functions as per the instructions

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+     
+Guok = Dog("Guok", "Mutina")
+print(Guok.name)
+print(Guok.breed)
+
+
+
